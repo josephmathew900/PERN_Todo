ebd812dfa0061002ef97c40397e85482a3b2a4ed
josephmathew900 <josephmathew900@gmail.com>
2021-03-03 21:12:33 +0530

    pg setup and create todo done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
+// MIDDLEWARE
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
+//ROUTES
 
-app.listen(500, () => {
-  console.log("Server has started on port 5000")
+//CREATE A TODO
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//GET ALL TODOS
+
+//GET A TODO
+
+//UPDATE A TODO
+
+//DELETE A TODO
+
+app.listen(5000, () => {
+  console.log("Server has started on port 5000");
 });
